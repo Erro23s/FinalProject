b0c1f8837721de458e080340b4a3e85fa227ab86
Felipe Drehmer de Souza <felipedreh@gmail.com>
2024-10-30 19:49:22 -0300

    Ta ai o fundo

--- AvaliadorMusicas/Views/musicas/show.php
+++ AvaliadorMusicas/Views/musicas/show.php
@@ -30,14 +30,11 @@
 
     
   </ul>
-  <form class="d-flex" role="search">
-    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success" type="submit">Search</button>
-  </form>
+  
 </div>
 </div>
 </nav>
-<div>
+<div class="fundo">
 
   <body>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -51,7 +48,7 @@
           </div>
           <div class="button">
             <i data-feather="edit"></i>
-            <p>Editar</p>
+            <p type="button">Editar</p>
           </div>
         </div>
         <div class="stats">
@@ -62,6 +59,7 @@
         </div>
       </div>
     </div>
+</div>
     <script>
       feather.replace();
     </script>
